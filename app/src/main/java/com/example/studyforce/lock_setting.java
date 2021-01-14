701b1227285c54ad589c2ab8a60d0286310afac3
paradohee <paradohee0518@gmail.com>
2021-01-14 23:17:26 +0900

    레이아웃 (완벽하지는 않음...)

---
+++ app/src/main/java/com/example/studyforce/lock_setting.java
@@ -0,0 +1,14 @@
+package com.example.studyforce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lock_setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_setting);
+    }
+}
---
+++ app/src/main/java/com/example/studyforce/my_class_list.java
@@ -0,0 +1,14 @@
+package com.example.studyforce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class my_class_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_class_list);
+    }
+}
