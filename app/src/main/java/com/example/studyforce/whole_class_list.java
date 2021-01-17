47fb44eff9060ee7a570347af3fdbf270734befc
paradohee <paradohee0518@gmail.com>
2021-01-17 21:11:22 +0900

    클래스리스트레이아웃추가

---
+++ app/src/main/java/com/example/studyforce/whole_class_list.java
@@ -0,0 +1,14 @@
+package com.example.studyforce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class whole_class_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whole_class_list);
+    }
+}
