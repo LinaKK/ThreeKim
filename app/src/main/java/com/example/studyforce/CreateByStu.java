7bef11b65b7476039ec5432296fd5136916d7d87
LinaaKK <linaa0112@gmail.com>
2021-01-20 01:02:40 +0900

    클래스 만들기 레이아웃 추가

--- app/src/main/java/com/example/studyforce/CreateByStu.java
+++ app/src/main/java/com/example/studyforce/CreateByStu.java
@@ -1,14 +1,47 @@
 package com.example.studyforce;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateByStu extends AppCompatActivity {
+    private String spw = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_by_stu);
+        updateResult();
     }
+
+    private void updateResult() {
+        TextView spw = (TextView) findViewById(R.id.spw);
+        spw.setText("비밀번호: "+spw);
+    }
+
+    public void mOnCLick(View V) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.secretpw, null);
+        builder.setView(layout);
+        final EditText editspw = (EditText) layout.findViewById(R.id.editspw);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                spw = editspw.getText().toString();
+                updateResult();
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.create().show();
+    }
+
 }
+
+
