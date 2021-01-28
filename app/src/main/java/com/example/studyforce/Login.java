a6fe18b8d9f4007373fd042eaab61dc553acdef1
wrtg61 <shk99061@naver.com>
2021-01-28 22:50:17 +0900

    로그인 java 미완성

--- app/src/main/java/com/example/studyforce/Login.java
+++ app/src/main/java/com/example/studyforce/Login.java
@@ -2,13 +2,38 @@ package com.example.studyforce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.studyforce.R;
 
 public class Login extends AppCompatActivity {
+    EditText et_id,et_pw;
+    String aId,aPw;
+    Button btn_lo,btn_sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
+
+        et_id=findViewById(R.id.et_id);
+        et_pw=findViewById(R.id.et_pw);
+        btn_lo=findViewById(R.id.btn_lo);
+        btn_sign=findViewById(R.id.btn1_sign);
+
     }
+    public void btn_lo(View view)
+    {
+
+    }
+
 }
---
+++ app/src/main/java/com/example/studyforce/Sign_Request.java
@@ -0,0 +1,14 @@
+package com.example.studyforce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_Request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign__request);
+    }
+}
