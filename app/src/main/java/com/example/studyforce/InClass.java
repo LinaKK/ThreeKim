e3e34509282dd9f901e7a306cc68c1b5bb962c1a
LinaaKK <linaa0112@gmail.com>
2021-02-02 00:21:31 +0900

    클래스내부

---
+++ app/src/main/java/com/example/studyforce/InClass.java
@@ -0,0 +1,14 @@
+package com.example.studyforce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_class);
+    }
+}
