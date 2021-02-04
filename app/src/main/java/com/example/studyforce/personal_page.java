6d2682b37608d09d0ee48c572a8dbd735cb25373
paradohee <paradohee0518@gmail.com>
2021-02-04 21:12:36 +0900

    개인페이지 버튼 누를 시 다른 액티비티 호출 완료

--- app/src/main/java/com/example/studyforce/personal_page.java
+++ app/src/main/java/com/example/studyforce/personal_page.java
@@ -1,14 +1,44 @@
 package com.example.studyforce;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class personal_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_page);
+        setContentView(R.layout.activity_personal_page); }
+    //개인정보
+    void onButton_info(View view){
+        Intent intent = new Intent(this, personal_info.class);
+        startActivity(intent);
+    }
+    //잠금설정
+    void onButton_lock(View v){
+        Intent intent = new Intent(this, lock_setting.class);
+        startActivity(intent);
+    }
+    //스케줄러
+    void onButton_schedule(View v){
+        Intent intent = new Intent(this, personal_schedule.class);
+        startActivity(intent);
+    }
+    //개인 클래스 리스트
+    void onButton_uclass(View v){
+        Intent intent = new Intent(this, my_class_list.class);
+        startActivity(intent);
+    }
+
+
+    //앱종료
+    void app_exit(View v){
+        ActivityCompat.finishAffinity(this);
+        System.exit(0);
     }
 }
