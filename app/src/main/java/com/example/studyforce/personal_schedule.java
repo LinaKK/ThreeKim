b0c0871c0190fa0dc47c0b84ca20ca9c156e4bd6
paradohee <paradohee0518@gmail.com>
2021-02-22 23:24:38 +0900

    캘린더 일정추가(1)

--- app/src/main/java/com/example/studyforce/personal_schedule.java
+++ app/src/main/java/com/example/studyforce/personal_schedule.java
@@ -1,8 +1,13 @@
 package com.example.studyforce;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class personal_schedule extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class personal_schedule extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_schedule);
+
+
+        final TextView date = findViewById(R.id.date);
+        CalendarView calendarView = findViewById(R.id.calendar);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                month +=1;
+                date.setText(String.format("%d년 %d월 %d일", year, month, day));
+            }
+        });
+
+
+    }
+
+    //일정 추가이벤트
+    public void date_add(View view) {
+
     }
 }
