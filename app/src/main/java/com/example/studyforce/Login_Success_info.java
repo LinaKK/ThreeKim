015bc27f87413c7bfca9a1b93e1ed85288163964
paradohee <paradohee0518@gmail.com>
2021-03-04 22:35:39 +0900

    로그인 성공 시 값 받아오기 및 기타...

--- app/src/main/java/com/example/studyforce/Login_Success_info.java
+++ app/src/main/java/com/example/studyforce/Login_Success_info.java
@@ -11,5 +11,6 @@ public class Login_Success_info {
 
 
     //나 intent 사용 안했어. 그냥 int 변수(=학번)만 사용해도 될것같아서.. 만약 int제외 다른 형태 변수가 필요하면 intent로 전환 시켜도 돼!!!
+    //이거는 activity에서 acitvity로 int변수만 넘길 때 사용하는 방법이래!!!
 
 }
