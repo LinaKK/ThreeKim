5614fc668069b2fde8013bbcca53511e5b65b2e0
wrtg61 <shk99061@naver.com>
2021-03-06 01:03:00 +0900

    회원가입 중복체크기능

--- app/src/main/java/com/example/studyforce/Sign.java
+++ app/src/main/java/com/example/studyforce/Sign.java
@@ -1,33 +1,89 @@
 package com.example.studyforce;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
-import android.os.Bundle;
+
 
 public class Sign extends AppCompatActivity {
 
     private EditText si_n, si_id, si_pw, si_pwc;
-    private Button
+    private Button btn1_sign, btn1_idc;
+    private AlertDialog dialog;
+    private boolean validate = false; //학번 중복체크 기능
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
 
-        //값찾기
+        //값찾기 이름,학번,비번,비밀번호확인
         si_n=findViewById(R.id.si_n);
         si_id=findViewById(R.id.si_id);
         si_pw=findViewById(R.id.si_pw);
         si_pwc=findViewById(R.id.si_pwc);
 
-        //연동 후에 중복체크 기능 넣기 user=si_n @Override
-        //회원가입 누를 시에 연동기능 넣기 (override 입력안하면 back)
+        btn1_sign=findViewById(R.id.btn1_sign);
+        btn1_sign.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                if (Uname.getText().toString().length ==0){
+                    Toast.makeText(Sign.this, "이름을 입력하세요", Toast.LENGTH_SHORT).show();
+                    Uname.requestFocus();
+                    return;
+                }
+                if (Uid.getText().toString().length ==0){
+                    Toast.makeText(Sign.this, "학번을 입력하세요", Toast.LENGTH_SHORT).show();
+                    Uid.requestFocus();
+                    return;
+                }
+                if (Upw.getText().toString().length ==0){
+                    Toast.makeText(Sign.this, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show();
+                    Upw.requestFocus();
+                    return;
+                }
+                if (Uemail.getText().toString().length ==0){
+                    Toast.makeText(Sign.this, "이메일을 입력하세요", Toast.LENGTH_SHORT).show();
+                    Uemail.requestFocus();
+                    return;
+                }
+                if(!Upwc.getText().toString().equals(Upw.getText().toString()) ) {
+                    Toast.makeText(Sign.this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    Upwc.setText("");
+                    Upwc.requestFocus();
+                    return;
+                }
+            }
+        });
+
+        btn1_idc =findViewById(R.id.btn1_idc);
+        btn1_idc.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String Uid=si_id.getText().toString();
+                if(validate){
+                    return; //중복체크 완료
+            }
+                if (Uid.equals("")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(Sign.this)
+
+                }
+        });
+
+
+        }
+
+
+        //추가할 것: 중복체크 보안, 서버 연동 후 이름 체크, 회원가입누르면 화면전환(로그인)
 
     }
+
 }
