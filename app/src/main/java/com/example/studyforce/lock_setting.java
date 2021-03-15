329ebd3ca87bb88b0fbec52d894e98cc55515abb
paradohee <paradohee0518@gmail.com>
2021-03-15 23:04:08 +0900

    클래스 리스트뷰 검색(2) &잠금화면(1)

--- app/src/main/java/com/example/studyforce/lock_setting.java
+++ app/src/main/java/com/example/studyforce/lock_setting.java
@@ -3,12 +3,22 @@ package com.example.studyforce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class lock_setting extends AppCompatActivity {
+
+    Button lock_on;
+    Button lock_off;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock_setting);
+
+        lock_off=(Button)findViewById(R.id.lock_off);
+        lock_on=(Button)findViewById(R.id.lock_on);
+
+
     }
 }
