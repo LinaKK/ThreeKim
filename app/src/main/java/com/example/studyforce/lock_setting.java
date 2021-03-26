c6d0fec49004b76874f7bd0fcea75dab002a4cfa
paradohee <paradohee0518@gmail.com>
2021-03-26 16:24:53 +0900

    오류투성이

--- app/src/main/java/com/example/studyforce/lock_setting.java
+++ app/src/main/java/com/example/studyforce/lock_setting.java
@@ -2,7 +2,9 @@ package com.example.studyforce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class lock_setting extends AppCompatActivity {
@@ -23,4 +25,8 @@ public class lock_setting extends AppCompatActivity {
     }
 
     //곧 없어질 액티비티
+    void onButton_info2(View view){
+        Intent intent = new Intent(this, InClass.class);
+        startActivity(intent);
+    }
 }
