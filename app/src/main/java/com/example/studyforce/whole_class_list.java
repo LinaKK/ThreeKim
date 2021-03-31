f918a61167b55d922b2901f18d998aa3e29b1c76
paradohee <paradohee0518@gmail.com>
2021-03-31 10:15:07 +0900

    수정

--- app/src/main/java/com/example/studyforce/whole_class_list.java
+++ app/src/main/java/com/example/studyforce/whole_class_list.java
@@ -52,71 +52,14 @@ public class whole_class_list extends AppCompatActivity {
 
 
         //클래스찾기버튼
-        fClass.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View view) {
-                //
-            }
-        });
 
         //클래스 추가버튼
-        addClass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mClassSelectDialog.show();
 
-            }
-        });
-
-        mClassSelectDialog = new AlertDialog.Builder(whole_class_list.this)
-                .setItems(mClass, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                })
-                .setTitle("신분")
-                .setPositiveButton("확인", null)
-                .setNegativeButton("취소",null)
-                .create();
 
         //리스트뷰 초기화
-        cList = (ListView)findViewById(R.id.wholeClass);
-        cLAdapter1 = new ClassLIstAdapter1(this, cJobsData);
-        cList.setAdapter(cLAdapter1);
 
-        //리스트 클릭 이벤트 처리 - 누르면 추가 여부 메시지 창 띄우기(alertdialog)
-        cList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                showMessage();
-
-            }
-        });
     }
     //메시지 창 설정
-    public void showMessage(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("과목 추가");
-        builder.setMessage("해당 클래스를 추가하시겠습니까?");
 
-        //추가 버튼 (눌리면 my_class_list에 추가됨)
-        builder.setPositiveButton("추가", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
-        });
-        //취소버튼
-        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
-        });
-
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
 }
