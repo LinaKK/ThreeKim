5da01e2e3cc25d88ca14e540e37997640fe2e6bf
paradohee <paradohee0518@gmail.com>
2021-04-24 22:37:32 +0900

    db로 넘기기 및 받아오기

--- app/src/main/java/com/example/studyforce/personal_info.java
+++ app/src/main/java/com/example/studyforce/personal_info.java
@@ -15,6 +15,10 @@ public class personal_info extends AppCompatActivity {
     Button exit;
     TextView uname;
     TextView uid;
+    TextView umail;
+    String username;
+    String usermail;
+    int userid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +31,20 @@ public class personal_info extends AppCompatActivity {
         //로그인 성공시 받아올 학번
         //int userid3 = ((Login_Request)Login_Request.context_login).userid1;
         /*
-         String username = jsonObject.getString("name"); //'pinfo'테이블
-         String usermail = jsonObject.getString("email"); //'pinfo'테이블
-         int userid = jsonObject.getint("num"); //'pinfo'테이블
+         username = jsonObject.getString("name"); //'pinfo'테이블
+         usermail = jsonObject.getString("email"); //'pinfo'테이블
+         userid = jsonObject.getint("num"); //'pinfo'테이블
          */
-
 
         uname = findViewById(R.id.uname);
         uid = findViewById(R.id.uid);
+        umail = findViewById(R.id.umail);
         exit = (Button)findViewById(R.id.exit3);
+
+        uname.setText(username);
+        uid.setText(userid);
+        umail.setText(usermail);
+
 
         //메인으로 나가기
         exit.setOnClickListener(new View.OnClickListener() {
