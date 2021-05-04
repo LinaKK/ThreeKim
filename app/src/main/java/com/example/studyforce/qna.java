bfe40ed5b17090de7674e9a6faf037c682f76c7e
LinaKK <lilikim0112@gmail.com>
2021-05-04 19:39:04 +0900

    //

---
+++ app/src/main/java/com/example/studyforce/qna.java
@@ -0,0 +1,19 @@
+package com.example.studyforce;
+
+public class qna {
+    int qnanum;
+    String qtitle;
+    String q;
+    String a;
+    String qwriter, awriter;
+
+    public qna( int qnanum, String qtitle, String q, String a, String qwriter, String awriter){
+        this.qnanum = qnanum;
+        this.qtitle = qtitle;
+        this.q = q;
+        this.a=a;
+        this.qwriter = qwriter;
+        this.awriter = awriter;
+
+    }
+}
--- app/src/main/java/com/example/studyforce/qnaList.java
+++ app/src/main/java/com/example/studyforce/qnaList.java
@@ -3,12 +3,20 @@ package com.example.studyforce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class qnaList extends AppCompatActivity {
+    private static qna qnalist[];
+    private ListView qlist;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qna_list);
+        qlist = (ListView) findViewById(R.id.qlist);
+
     }
 }
