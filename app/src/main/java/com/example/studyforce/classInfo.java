9c0ff440c0218f2eadef0edb5ba1c139e376872b
paradohee <paradohee0518@gmail.com>
2021-05-18 23:47:34 +0900

    클래스 정보 메뉴 삽입

--- app/src/main/java/com/example/studyforce/classInfo.java
+++ app/src/main/java/com/example/studyforce/classInfo.java
@@ -3,12 +3,30 @@ package com.example.studyforce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class classInfo extends AppCompatActivity {
+    TextView cSub;
+    ListView cPList;
+    Button exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_info);
+
+        cSub = (TextView)findViewById(R.id.cSubject);
+        cPList = (ListView)findViewById(R.id.cPersonList);
+
+        exit = (Button)findViewById(R.id.exit4);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
